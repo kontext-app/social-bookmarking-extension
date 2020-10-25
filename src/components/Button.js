7f8af9508ffa5d7853d67c2932b63b8d47b869bf
Dong-Ha Kim <dongha.kim210@gmail.com>
2020-10-25 13:05:59 +0100

    Add Button component

---
+++ src/components/Button.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import { space, color, layout, flexbox, background, border, position, shadow } from 'styled-system';
+
+export const Button = styled.button(
+  {
+    backgroundColor: props => props.theme.colors.background,
+    color: props => props.theme.colors[props.type],
+    border: props => `2px solid ${props.theme.colors[props.type]}`,
+    borderRadius: 3,
+    padding: '8px 16px',
+    fontFamily: props => props.theme.fonts.body,
+    fontSize: '16px',
+    '&:hover': {
+      opacity: 0.5,
+      cursor: 'pointer',
+    },
+    fontWeight: 'bold',
+    boxShadow: 'none',
+  },
+  space,
+  color,
+  layout,
+  flexbox,
+  background,
+  border,
+  position,
+  shadow
+);
+
+Button.defaultProps = {
+  type: 'primary',
+};
