8566016d1fb99c7b0837ced1ed118092bded7d96
Dong-Ha Kim <dongha.kim210@gmail.com>
2020-11-15 18:19:16 +0100

    Add ceramic helpers for creating a bookmark

--- src/apis/ceramic.js
+++ src/apis/ceramic.js
@@ -5,6 +5,13 @@ import { PUBLISHED_DEFINITIONS, PUBLISHED_SCHEMAS } from 'constants/definitions'
 
 let idx;
 
+export const DefaultBookmarksIndexKeys = {
+  UNSORTED: 'unsorted',
+  PUBLIC: 'public',
+  PRIVATE: 'private',
+  LISTS: 'lists',
+};
+
 export function createIDX() {
   idx = new IDXWeb({
     ceramic: process.env.CERAMIC_API_HOST,
@@ -31,4 +38,97 @@ export function isIDXAuthenticated() {
 
 export function getDID() {
   return idx.id;
+}
+
+export async function getProfileByDID(did) {
+  return idx.get('basicProfile', did);
+}
+
+export async function getBookmarksIndexDocID(did) {
+  const idxDocContent = await idx.getIDXContent(did);
+
+  if (!idxDocContent) {
+    return null;
+  }
+
+  const bookmarksIndexDocID = idxDocContent[PUBLISHED_DEFINITIONS.BookmarksIndex];
+  return bookmarksIndexDocID;
+}
+
+export async function getBookmarksIndexDocContent(did) {
+  return idx.get('BookmarksIndex', did);
+}
+
+export async function hasBookmarksIndex(did) {
+  return idx.has('BookmarksIndex', did);
+}
+
+export async function setDefaultBookmarksIndex() {
+  const defaultBookmarksIndexKeyToDocID = {};
+
+  for (const defaultBookmarksIndexKey of Object.values(DefaultBookmarksIndexKeys)) {
+    const docID =
+      defaultBookmarksIndexKey === DefaultBookmarksIndexKeys.LISTS
+        ? await createEmptyBookmarksListsDoc()
+        : await createEmptyBookmarksDoc();
+    defaultBookmarksIndexKeyToDocID[defaultBookmarksIndexKey] = docID;
+  }
+
+  await idx.remove('BookmarksIndex');
+  const bookmarksIndexDocID = await idx.set('BookmarksIndex', defaultBookmarksIndexKeyToDocID);
+
+  return bookmarksIndexDocID.toUrl('base36');
+}
+
+export async function createEmptyBookmarksDoc() {
+  const { id } = await idx.ceramic.createDocument('tile', {
+    content: [],
+    metadata: {
+      schema: PUBLISHED_SCHEMAS.Bookmarks,
+      controllers: [getDID()],
+      tags: ['bookmarks'],
+      isUnique: true,
+    },
+  });
+
+  return id.toUrl('base36');
+}
+
+export async function createEmptyBookmarksListsDoc() {
+  const { id } = await idx.ceramic.createDocument('tile', {
+    content: [],
+    metadata: {
+      schema: PUBLISHED_SCHEMAS.BookmarksLists,
+      controllers: [getDID()],
+      tags: ['bookmarks'],
+      isUnique: true,
+    },
+  });
+
+  return id.toUrl('base36');
+}
+
+export async function createBookmarkDoc(bookmarkToAdd) {
+  const { id } = await idx.ceramic.createDocument('tile', {
+    content: bookmarkToAdd,
+    metadata: {
+      schema: PUBLISHED_SCHEMAS.Bookmark,
+      controllers: [getDID()],
+      tags: ['bookmarks'],
+    },
+  });
+  return id.toUrl('base36');
+}
+
+export async function addBookmarkDocToBookmarksDoc(bookmarkDocID, bookmarksDocID) {
+  const bookmarksDoc = await idx.ceramic.loadDocument(bookmarksDocID);
+  const existingBookmarkDocIDs = bookmarksDoc.content;
+  const updatedBookmarkDocIDs = [bookmarkDocID, ...existingBookmarkDocIDs];
+
+  await bookmarksDoc.change({
+    content: updatedBookmarkDocIDs,
+  });
+
+  const updatedBookmarksDoc = await idx.ceramic.loadDocument(bookmarksDocID);
+  return updatedBookmarksDoc;
 }
--- src/features/popup/utils.js
+++ src/features/popup/utils.js
@@ -10,3 +10,23 @@ export async function sendMessageToActiveTab(message) {
 
   return browser.tabs.sendMessage(activeTabs[0].id, message);
 }
+
+export function enrichPartialBookmark(partialBookmark) {
+  const {
+    url = '',
+    description = '',
+    title = '',
+    author = '',
+    highlightedText = '',
+    creationDate = new Date().toISOString(),
+  } = partialBookmark;
+
+  return {
+    url,
+    title,
+    author,
+    description,
+    highlightedText,
+    creationDate,
+  };
+}
